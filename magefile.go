a80ddcd8cc8f2d9999d1d27fa2b03d9f089788e7
Reinhard Naegele <unguiculus@gmail.com>
2020-03-06 11:13:56 +0100

    Update readme
    Signed-off-by: Reinhard Naegele <unguiculus@gmail.com>

--- magefile.go
+++ magefile.go
@@ -7,29 +7,29 @@ import (
 )
 
 func Lint() error {
-	if err := sh.Run("golangci-lint", "run"); err != nil {
+	if err := sh.RunV("golangci-lint", "run"); err != nil {
 		return err
 	}
-	if err := sh.Run("go", "vet", "-v", "./..."); err != nil {
+	if err := sh.RunV("go", "vet", "-v", "./..."); err != nil {
 		return err
 	}
-	if err := sh.Run("goimports", "-w", "-l", "."); err != nil {
+	if err := sh.RunV("goimports", "-w", "-l", "."); err != nil {
 		return err
 	}
-	if err := sh.Run("go", "mod", "tidy"); err != nil {
+	if err := sh.RunV("go", "mod", "tidy"); err != nil {
 		return err
 	}
-	return sh.Run("git", "diff", "--exit-code")
+	return sh.RunV("git", "diff", "--exit-code")
 }
 
 func Test() error {
-	return sh.Run("go", "test", "./...", "-race")
+	return sh.RunV("go", "test", "./...", "-race")
 }
 
 func Build() error {
-	return sh.Run("goreleaser", "release", "--rm-dist", "--snapshot")
+	return sh.RunV("goreleaser", "release", "--rm-dist", "--snapshot")
 }
 
 func Release() error {
-	return sh.Run("goreleaser", "release", "--rm-dist")
+	return sh.RunV("goreleaser", "release", "--rm-dist")
 }
